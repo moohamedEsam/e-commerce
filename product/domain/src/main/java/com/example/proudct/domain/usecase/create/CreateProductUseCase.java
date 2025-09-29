271512e92071c7d9d43fda7e52bc078970f4a72c
mohamedEsam <moohamedEsam0@gmail.com>
2025-09-29 10:32:42 +0300

    Remove `@FunctionalInterface` annotation from `CreateProductUseCase`

--- product/domain/src/main/java/com/example/proudct/domain/usecase/create/CreateProductUseCase.java
+++ product/domain/src/main/java/com/example/proudct/domain/usecase/create/CreateProductUseCase.java
@@ -3,7 +3,6 @@ package com.example.proudct.domain.usecase.create;
 import com.example.proudct.domain.model.Product;
 import reactor.core.publisher.Mono;
 
-@FunctionalInterface
 public interface CreateProductUseCase {
     Mono<Product> execute(Product product);
 }
